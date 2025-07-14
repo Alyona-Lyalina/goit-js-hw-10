73bfc0e191ca95eae00fc295be44c6e37bd759b7
Alyona Lyalina <lyalina.alyona@icloud.com>
2025-07-14 23:02:55 +0300

    Update resource paths

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,2 +1,13 @@
 import flatpickr from "flatpickr";
 import iziToast from "izitoast";
+
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
